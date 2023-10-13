3e88ec9fb048689ad82c3b36059cea6bf70c9c79
Juan Rodriguez (yonailo) <juanfrancisco.rodriguezhervella@atos.net>
2023-10-13 08:57:10 +0100

    Added missing configuration

---
+++ after.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
+#
+# If you have user-specific configurations you would like
+# to apply, you may also create user-customizations.sh,
+# which will be run after this script.
+
+
+# If you're not quite ready for the latest Node.js version,
+# uncomment these lines to roll back to a previous version
+
+# Remove current Node.js version:
+#
+#   sudo apt-get -y purge nodejs
+#   sudo rm -rf /usr/lib/node_modules/npm/lib
+#   sudo rm -rf //etc/apt/sources.list.d/nodesource.list
+
+# Install Node.js Version desired (i.e. v13)
+# More info: https://github.com/nodesource/distributions/blob/master/README.md#debinstall
+#
+#   curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+#   sudo apt-get install -y nodejs
+
+###################################################
+# THIS IS TO INCREASE MYSQL DISK ESPACE
+###################################################
+#
+# lvextend -L20G -r homestead-vg/mysql-master
+
+###################################################
+# This is to update the PUBLICKEY of heroku reopsitory
+###################################################
+#
+# curl https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+
+###################################################
+# share local site with internet
+###################################################
+#
+# tailscale serve https / http://127.0.0.1:8080
+
+sudo bash -c 'echo "none" > /sys/block/sda/queue/scheduler'
